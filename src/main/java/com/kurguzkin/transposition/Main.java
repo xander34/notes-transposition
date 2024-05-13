5d34916ce385c2ee27dd31771ac19465d7be655e
xander34 <xander34@gmail.com>
2024-05-13 19:35:41 +0300

    Fixing error message

--- src/main/java/com/kurguzkin/transposition/Main.java
+++ src/main/java/com/kurguzkin/transposition/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
 
         if (args.length < 2) {
-            System.out.println("Usage: java -jar target/notes-transposition-1.0-SNAPSHOT-jar-with-dependencies.java <list> <shift>");
+            System.out.println("Usage: java -jar target/transpose.java <list> <shift>");
             return;
         }
 
